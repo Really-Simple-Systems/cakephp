68449539484f2830c1be083a08657619295b81b2
Mark Story <mark@mark-story.com>
2023-09-08 00:20:19 -0400

    Fix regression for non-ascii addresses
    https://datatracker.ietf.org/doc/html/rfc2047#section-5
    
    States that 'An encoded-word' MUST NOT appear within 'quoted-string'.
    
    https://datatracker.ietf.org/doc/html/rfc5322#section-3.4
    
    states that 'display-name' is a 'phrase' which is compsed of 'word'
    and 'word' is made of atom / quoted-string.

--- src/Mailer/Message.php
+++ src/Mailer/Message.php
@@ -999,7 +999,7 @@ class Message implements JsonSerializable, Serializable
                 $return[] = $email;
             } else {
                 $encoded = $this->encodeForHeader($alias);
-                if (preg_match('/[^a-z0-9+\-\\=? ]/i', $encoded)) {
+                if ($encoded === $alias && preg_match('/[^a-z0-9+\-\\=? ]/i', $encoded)) {
                     $encoded = '"' . addcslashes($encoded, '"\\') . '"';
                 }
                 $return[] = sprintf('%s <%s>', $encoded, $email);
