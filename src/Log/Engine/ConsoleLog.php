04155ac514f3c8ade8ce294f8205bfd0cc63cccc
Matt Ostrom-Hall <matt.ostrom-hall@reallysimplesystems.com>
2025-03-07 11:46:30 +0000

    fixed upgrade issues

--- src/Log/Engine/ConsoleLog.php
+++ src/Log/Engine/ConsoleLog.php
@@ -78,13 +78,13 @@ class ConsoleLog extends BaseLog
      * @param string $level The severity level of log you are making.
      * @param string $message The message you want to log.
      * @param array $context Additional information about the logged message
-     * @return bool success of write.
+     * @return void success of write.
      */
-    public function log($level, $message, array $context = [])
+    public function log($level, $message, array $context = []): void
     {
         $message = $this->_format($message, $context);
         $output = date('Y-m-d H:i:s') . ' ' . ucfirst($level) . ': ' . $message;
 
-        return (bool)$this->_output->write(sprintf('<%s>%s</%s>', $level, $output, $level));
+        $this->_output->write(sprintf('<%s>%s</%s>', $level, $output, $level));
     }
 }
